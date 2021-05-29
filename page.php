ecc0f2d8012961ad48a38faf91873473d46e4c51
Blaine Lafreniere <brlafreniere@gmail.com>
2021-05-29 13:52:08 -0600

    - add page.php template (page doesn't output title in content) - add singular.php template - some style updates (colors)

--- page.php
+++ page.php
@@ -1,12 +1,10 @@
 <?php get_header() ?>
 
 <div class="container" id='content-container'>
-    <h3>Recent Blog Posts</h3>
     <?php while (have_posts()) { ?>
         <div id="post-<?php the_ID() ?>" <?php post_class() ?>>
             <?php the_post(); ?>
-
-            <a href="<?php echo get_permalink() ?>"><?php the_title() ?></a>
+            <?php the_content(); ?>
         </div>
     <?php } ?>
 </div>
--- singular.php
+++ singular.php
@@ -1,12 +1,13 @@
 <?php get_header() ?>
 
 <div class="container" id='content-container'>
-    <h3>Recent Blog Posts</h3>
     <?php while (have_posts()) { ?>
         <div id="post-<?php the_ID() ?>" <?php post_class() ?>>
             <?php the_post(); ?>
 
-            <a href="<?php echo get_permalink() ?>"><?php the_title() ?></a>
+            <h1 class="mb-5 mt-5"><?php the_title() ?></h1>
+
+            <?php the_content(); ?>
         </div>
     <?php } ?>
 </div>
